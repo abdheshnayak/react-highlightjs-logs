0a063c0ea2fdeb270d421418de2f33ec10774b72
Abdhesh Nayak <abdheshnayak@gmail.com>
2022-08-08 12:43:59 +0530

    :rocket: deploy

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.scss';
-import './themes/tokyo-night.css';
+import './css/deply.scss';
+// import './css/build.scss';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
