cacca6944798e854c174e23032f44ab46ec433c0
Abdhesh Nayak <abdheshnayak@gmail.com>
2022-08-08 12:46:25 +0530

    :rocket: deploy

--- src/App.js
+++ src/App.js
@@ -2,17 +2,17 @@ import { HighlightJsLogs } from "./lib"
 function App() {
   return (
     <div className="min-h-screen bg-orange-500">
-<div className="p-6 min-h-screen flex flex-col">
-      <HighlightJsLogs 
-        maxHeight="calc(100vh - 9rem)"
-        noScrollBar
-        title="Last 24 hours logs" 
-        url={`/logs.txt`} 
-        fontSize={14}
-        // maxLines={100}
-        loadingComponent={<span>Loading...</span>}
-       />
-    </div>
+      <div className="p-6 flex flex-col">
+        <HighlightJsLogs 
+          maxHeight="calc(100vh - 8rem)"
+          noScrollBar
+          title="Last 24 hours logs" 
+          url={`/logs.txt`} 
+          fontSize={14}
+          // maxLines={100}
+          loadingComponent={<span>Loading...</span>}
+        />
+      </div>
     </div>
     
   );
